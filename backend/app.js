887ed9cb7a96b2d1d41f9f5b896e546e7952f541
Hammad Ansari <ansarihamad123@gmail.com>
2024-11-25 21:47:53 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ require("dotenv").config();
 
 
 app.use(cors({
-    origin: "http://localhost:3000", 
+    origin: "https://mern-ecommerce-project-frontend-alpha.vercel.app", 
     credentials: true 
 }));
 
